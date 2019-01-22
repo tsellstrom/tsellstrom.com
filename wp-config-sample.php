1c5ec7c3683f6f85959396bda7f8e0c9abf5e8b7
Tom Sellstrom <tom@tsellstrom.com>
2019-01-22 11:14:42 -0600

    Updating wordpress to latest version

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'tsellstrom');
+define('DB_NAME', 'database_name_here');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'username_here');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'password_here');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -37,10 +37,6 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-define('WP_SITEURL', 'http://local.tsellstrom');
-
-define('WP_HOME',    'http://local.tsellstrom');
-
 /**#@+
  * Authentication Unique Keys and Salts.
  *
@@ -50,14 +46,14 @@ define('WP_HOME',    'http://local.tsellstrom');
  *
  * @since 2.6.0
  */
- define('AUTH_KEY',         '@sTMBN-w/UL,IIO~b (vMe2Y#!w@Xgd+Y0vdbr3[-.{Kgo@6p!KybD,YH-T=c[Em');
- define('SECURE_AUTH_KEY',  '@]Gu*{?L<fWZ^ZxAf|M4)T4,<6>#j)7w=|>-nQ}oX9Mj+)`-yK N@:?^4QC~qK`r');
- define('LOGGED_IN_KEY',    'sdhH0Cv+,zYo6ANO`8&|hyxgqR;&ei`45U~D-qZsDzm-.?-1P-$SSKl_i6Su^V4H');
- define('NONCE_KEY',        'h|>9~-G7xiZ.BqyRlj8u$^!o!@Xz~JFXm!~wP[c%S{bSI-_RZ1Oo.V3/z-IbM!%q');
- define('AUTH_SALT',        '9d HDYyJ8$8xrbP9wZk*98AZ|e3+j0I57Hb-u(!Q6H0{!~|rbdeNXN!uDw5DRvu:');
- define('SECURE_AUTH_SALT', '@hGr,}g!sV:*(VQv_C,qJQ+xj039[U&7Szw.x;`w.@BA1zz;/jT-]al}]PzjFgWI');
- define('LOGGED_IN_SALT',   'Q%!(ggV&h%`4=8pa]<yi1Drw,@5RheG++q*z([pdT.1&uD0n6Utq+[#lv}((-:Y ');
- define('NONCE_SALT',       '-=^T|dX^}qf!I3Cv_UeBioFaWW`pLxhf|_iqm&PB<a=U![MibO|]jpuAv.=R`r,u');
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
 /**#@-*/
 
